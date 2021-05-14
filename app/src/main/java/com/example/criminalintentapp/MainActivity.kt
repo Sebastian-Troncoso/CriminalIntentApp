3c9d45d1e6a227daf7eed4a8ab691df1ba3c739a
sebastiantroncoso <sctroncoso10@gmail.com>
2021-05-13 23:19:39 -0400

    Adding fragment to the fragmentManager's list of fragments

--- app/src/main/java/com/example/criminalintentapp/MainActivity.kt
+++ app/src/main/java/com/example/criminalintentapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.criminalintentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.criminalintentapp.controller.CrimeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Adding fragment to the fragmentManager's list of fragments
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
