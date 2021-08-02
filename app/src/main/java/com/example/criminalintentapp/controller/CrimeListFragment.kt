f48111235864de41e29aa556189d5bbfd2bd4a2c
sebastiantroncoso <sctroncoso10@gmail.com>
2021-08-02 15:24:21 -0400

    Adding images and updating the dependencies

--- app/src/main/java/com/example/criminalintentapp/controller/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintentapp/controller/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -51,11 +52,12 @@ class CrimeListFragment : Fragment() {
      * ViewHolder wires up the itemView with necessary field in the holder itself and returns the
      * holder back to the adapter so data can bind into these fields
      */
-    private inner class CrimeHolder(view: View)
-        : RecyclerView.ViewHolder(view), View.OnClickListener {
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -65,10 +67,15 @@ class CrimeListFragment : Fragment() {
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View) {
-            Toast.makeText(context,"${crime.title} pressed!",Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
         }
     }
 
