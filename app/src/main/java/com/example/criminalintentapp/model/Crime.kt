d1336d26b6916146bbd8fdcb339057cedbf49083
sebastian_troncoso <sctroncoso10@gmail.com>
2021-08-02 16:14:08 -0400

    Improving list_item_crime and DateFormat

--- app/src/main/java/com/example/criminalintentapp/model/Crime.kt
+++ app/src/main/java/com/example/criminalintentapp/model/Crime.kt
@@ -1,11 +1,12 @@
 package com.example.criminalintentapp.model
 
+import java.text.DateFormat
 import java.util.*
 
 data class Crime(
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
-    var date: Date = Date(),
+    var date: String = DateFormat.getDateTimeInstance().format(Date()),
     var isSolved: Boolean = false
 )
 
